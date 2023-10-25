73818b3eeee8318f6e4b64d9c39a3dffe7df451e
davaadorj battulga <davaa0910@gmail.com>
2023-10-25 13:14:38 +0200

    added start script

---
+++ start.sh
@@ -0,0 +1,11 @@
+USER=$(whoami)
+
+#teardown
+docker-compose down --remove-orphans
+
+#start
+docker-compose -f docker-compose.yml up -d --build
+docker-compose logs -f
+
+#echo "Type 'docker-compose down' to tear-down"
+#echo "Type 'docker-compose logs -f' to show logs."
