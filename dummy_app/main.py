aa637d9798228ed374708f7dce4c4e023e630201
davaadorj battulga <davaa0910@gmail.com>
2023-10-25 13:08:06 +0200

    initial

---
+++ dummy_app/main.py
@@ -0,0 +1,12 @@
+import argparse
+import logging
+import sys
+import os
+
+import datetime
+import logging
+
+
+if __name__ == '__main__':
+    time = datetime.datetime.now()
+    logging.warning(f'This log is from dockerized dummy thingsboard app, ran on {time}')
