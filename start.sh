32c9c10db9d921f036e01ddac1c47dea2a4803a3
davaadorj battulga <davaa0910@gmail.com>
2023-10-26 15:58:13 +0200

    edited start script

--- start.sh
+++ start.sh
@@ -5,7 +5,9 @@ docker-compose down --remove-orphans
 
 #start
 docker-compose -f docker-compose.yml up -d --build
-echo "$(docker-compose logs -f)"
+
+result = $(docker-compose logs -f)
+echo $result
 
 #echo "Type 'docker-compose down' to tear-down"
 #echo "Type 'docker-compose logs -f' to show logs."
