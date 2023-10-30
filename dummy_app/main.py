bb5f42737d3f24eea84d35d649f104992f34d30a
davaadorj battulga <davaa0910@gmail.com>
2023-10-30 10:10:18 +0100

    modifying logs

--- dummy_app/main.py
+++ dummy_app/main.py
@@ -2,12 +2,16 @@ import datetime
 import logging
 import time as t
 
+logger = logging.getLogger(__name__)
 
 def printer() -> str:
     current_time = datetime.datetime.now()
     something = (f"This string is returned from dockerized dummy things board app, "
                  f"indicating that it returned something, ran on {current_time}")
-
+    logger.error(f"This string is the log from dockerized dummy things board app "
+                 f"indicating that it returned something, ran on {current_time}")
+    print(f"This is the print statement from dockerized dummy things board app "
+                 f"indicating that it returned something, ran on {current_time}")
     return something
 
 
