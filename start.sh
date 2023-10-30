3282734495666434da637305422e1b89c7e468b1
davaadorj battulga <davaa0910@gmail.com>
2023-10-30 11:06:12 +0100

    editing permissions for dood

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 USER=$(whoami)
 
+sudo usermod -aG docker $USER                  # to add myself to docker group
+sudo chgrp docker /usr/local/bin/docker-compose     # to give docker-compose to docker group,
+sudo chmod 750 /usr/local/bin/docker-compose
+
 #teardown
 docker-compose down --remove-orphans
 
